b284161166917ed3815136077a104dcf109eb666
nevzery <goldsteampriem@gmail.com>
2026-06-26 00:34:54 +0300

    Update copyright year and owner in pin_gen.py

--- pin_gen.py
+++ pin_gen.py
@@ -1,5 +1,5 @@
 # PIN Generator — secure PIN generator
-# Copyright (C) 2024  Your Name
+# Copyright (C) 2026  nzteam
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
